1ba23e2b1446a0b642402099a6892adbeb7bc389
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-08-16 11:44:22 +0530

    warning corrected

--- littlebook/src/containers/sidebar/Sidebar.jsx
+++ littlebook/src/containers/sidebar/Sidebar.jsx
@@ -1,56 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import styles from './Sidebar.module.scss'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./Sidebar.module.scss";
 
-import { getUsers } from '../../services/data'
-import { FILTER, TITLE } from '../../constants/GeneralConstant'
-import Button from '../../components/button/Button'
-import { DARK_MODE, LIGHT_MODE, VIEW_MEMBERS } from '../../constants/ButtonConstant'
-import { extractFirstWord } from '../../utils/util'
-import CheckBox from '../../components/checkBox/CheckBox'
-import { useDispatch, useSelector } from 'react-redux'
-import { ThemeAction } from '../../store/ThemeSlice'
-import { ModalAction } from '../../store/ModalSlice'
+import { getUsers } from "../../services/data";
+import { FILTER, TITLE } from "../../constants/GeneralConstant";
+import Button from "../../components/button/Button";
+import {
+  DARK_MODE,
+  LIGHT_MODE,
+  VIEW_MEMBERS,
+} from "../../constants/ButtonConstant";
+import { extractFirstWord } from "../../utils/util";
+import CheckBox from "../../components/checkBox/CheckBox";
+import { useDispatch, useSelector } from "react-redux";
+import { ThemeAction } from "../../store/ThemeSlice";
+import { ModalAction } from "../../store/ModalSlice";
 
-const Sidebar = props => {
-
+const Sidebar = (props) => {
   const dispatch = useDispatch();
 
-  const theme = useSelector(state => state.theme.mode);
-  const show = useSelector(state => state.modal.isshowMemberModal);
-  const filterType = useSelector(state => state.blog.filterType);
+  const theme = useSelector((state) => state.theme.mode);
+  const show = useSelector((state) => state.modal.isshowMemberModal);
+  const filterType = useSelector((state) => state.blog.filterType);
   const onToggleModeHandler = () => {
     dispatch(ThemeAction.toggle());
-  }
+  };
   const onViewMemberHandler = () => {
     dispatch(ModalAction.showMemberModal());
+  };
 
-  }
-
-  const filters = filterType?.map((item) => <CheckBox label={item.type} isChecked={ item.isChecked} />)
+  const filters = filterType?.map((item) => (
+    <CheckBox key={item.type} label={item.type} isChecked={item.isChecked} />
+  ));
 
   return (
     <div className={styles["sidebar"]}>
+      <h1>{extractFirstWord(TITLE)}</h1>
 
-          <h1>{ extractFirstWord(TITLE)}</h1>
-          
-          <div className={styles["filter"]}>
-              <h2>{FILTER}  </h2>
-        <div>
-          
-                 {filters}
-                  </div>
-          </div>
-<div>
-        <Button name={VIEW_MEMBERS.name} styleName="sidebar-btn"  onClick={onViewMemberHandler} />
-              <Button name={theme==='light'? DARK_MODE.name:LIGHT_MODE.name} styleName="sidebar-btn" onClick={ onToggleModeHandler} />
-              </div>
+      <div className={styles["filter"]}>
+        <h2>{FILTER} </h2>
+        <div>{filters}</div>
+      </div>
+      <div>
+        <Button
+          name={VIEW_MEMBERS.name}
+          styleName="sidebar-btn"
+          onClick={onViewMemberHandler}
+        />
+        <Button
+          name={theme === "light" ? DARK_MODE.name : LIGHT_MODE.name}
+          styleName="sidebar-btn"
+          onClick={onToggleModeHandler}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-Sidebar.propTypes = {
+Sidebar.propTypes = {};
 
-}
-
-export default Sidebar
+export default Sidebar;
