e667b6e17399dd9c3fbd5798a46fe345928b956d
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-08-17 16:27:27 +0530

    Delete littlebook/src directory

--- littlebook/src/constants/NewBlogConstant.js
+++
@@ -1,6 +0,0 @@
-export const ADD_NEW_BLOG = {
-    title: "Add New Blog",
-    name: "Name your blog",
-    description:"Write Content here.."
-}
-
--- littlebook/src/services/data.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export const getBlogs = async () => {
-   return  await axios.get('https://jsonmockserver.vercel.app/api/blogs').then((data)=>data.data).catch((err)=>console.log(err));
-}
-
-export const getUsers = async () => {
-    return  await axios.get('https://jsonmockserver.vercel.app/api/users').then((data)=>data.data).catch((err)=>console.log(err));
- }
